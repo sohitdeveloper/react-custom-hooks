914aacd3a52868264961b0b4e08e99dae55654fb
Sohit Kumar <79898903+sohitdeveloper@users.noreply.github.com>
2023-06-30 13:22:50 +0530

    Create useToggle.js

---
+++ useToggle.js
@@ -0,0 +1,36 @@
+/* useToggle is a custom React hook that allows a component to toggle a value between true and false.
+It uses the useState hook to manage its state. First, the hook accepts a defaultValue argument to initialize the value state. 
+Then, it returns an array with two elements: the current value and a function called toggleValue that toggles
+the value between true and false. The function accepts one parameter. 
+It sets the value to the parameter If the parameter is boolean. Otherwise, it toggles the current value.*/
+
+import { useState } from "react"
+
+export default function useToggle(defaultValue) {
+  const [value, setValue] = useState(defaultValue)
+
+  function toggleValue(value) {
+    setValue(currentValue =>
+      typeof value === "boolean" ? value : !currentValue
+    )
+  }
+
+  return [value, toggleValue]
+}
+
+/* Here is an example of how you can use this hook: */
+
+import useToggle from "./hooks/useToggle"
+
+export default function ToggleComponent() {
+  const [value, toggleValue] = useToggle(false)
+
+  return (
+    <div>
+      <div>{value.toString()}</div>
+      <button onClick={toggleValue}>Toggle</button>
+      <button onClick={() => toggleValue(true)}>Make True</button>
+      <button onClick={() => toggleValue(false)}>Make False</button>
+    </div>
+  )
+}
